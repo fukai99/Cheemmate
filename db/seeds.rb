3d046c456cb6364afddd288e03eb39f3487735f5
fukai99 <141290272+fukai99@users.noreply.github.com>
2023-11-12 17:59:57 +0000

    fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,24 @@ Admin.find_or_create_by!(email: "123@456") do |admin|
   admin.password = "123456"
 end
 
-a_genre = Genre.find_or_create_by!(name: "A")
+# a_genre = Genre.find_or_create_by!(name: "A")
 
-yamada = User.find_or_create_by!(email: "aiu@aiu") do |user|
-  user.password = "123456"
-  user.family_name = "山田"
-  user.first_name = "やまだ"
-  user.disply_name = "ヤマダ"
-  user.is_member = false
-  user.family_relationship = "親"
-end
+# yamada = User.find_or_create_by!(email: "aiu@aiu") do |user|
+#   user.password = "123456"
+#   user.family_name = "山田"
+#   user.first_name = "やまだ"
+#   user.disply_name = "ヤマダ"
+#   user.is_member = false
+#   user.family_relationship = "親"
+# end
 
-hatsu_post = Post.find_or_create_by!(content: "初投稿です") do |post|
-  post.user = yamada
-  post.genre = a_genre
-end
+# hatsu_post = Post.find_or_create_by!(content: "次の振り付け共有します。") do |post|
+#   post.user = yamada
+#   post.genre = a_genre
+# end
 
-YoutubeUrl.find_or_create_by!(path: "OJmSwcww-pw", post_id: hatsu_post.id)
-# YoutubeUrl.find_or_create_by!(path: "iBDPW82ouCY", post_id: hatsu_post.id)
+# YoutubeUrl.find_or_create_by!(path: "b7B-F1O-AE8", post_id: hatsu_post.id)
+
+
 
 puts "データの作成が完了しました"
